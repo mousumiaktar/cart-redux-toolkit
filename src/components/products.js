15eeb90492d502898cb0f22083aedc2e127c6044
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2022-09-24 16:23:49 +0600

    products add

--- src/components/products.js
+++ src/components/products.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import { useEffect } from 'react';
+import  {useState}  from 'react';
 
-const products = () => {
+const Products = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(()=>{
+        const fetchProducts = async()=>{
+            const res = await fetch('https://fakestoreapi.com/products');
+             const data = await res.json();
+             console.log(data);
+             setProducts(data);
+        };
+         fetchProducts();
+    },[]);
+
     return (
-        <div>
-            
+        <div className="productsWrapper">
+            {products.map((product) => (
+                <div className="card" key={product.id}>
+                    <img src={product.image} alt="" />
+                    <h4>{product.title}</h4>
+                    <h5>{product.price}</h5>
+                    <button>
+                        Add to cart
+                    </button>
+                </div>
+            ))}
         </div>
     );
 };
 
-export default products;
+export default Products;
